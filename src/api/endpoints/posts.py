54d4dd8ef1916dbd043bbbc47bad61c5963cdcca
Yurb <yavurb@gmail.com>
2023-10-03 23:51:38 -0400

    ♻️  refactor: remove response schema
    Delete unnecessary assignment of response_model from create-post operation

--- src/api/endpoints/posts.py
+++ src/api/endpoints/posts.py
@@ -11,7 +11,7 @@ from schemas.post import Post, PostCreate
 router = APIRouter()
 
 
-@router.post("/", response_model=Post)
+@router.post("/")
 def create_post(post: PostCreate, db: Annotated[Session, Depends(deps.get_db)]) -> Post:
     post_created = crud.post.create(db, obj_in=post)
     return post_created
