c2d498f28c4ca123e1bc94f174b3d65613a2436e
Yurb <yavurb@gmail.com>
2023-10-07 21:22:41 -0400

    🗂️ feat: add database model
    Create Project model
    
    Add new revision for the project model. Upgrade db to the latest revision.

--- src/models/base.py
+++ src/models/base.py
@@ -1,2 +1,3 @@
 from src.database.base import Base  # noqa
 from src.models.post import Post  # noqa
+from src.models.project import Project  # noqa
--- src/models/project.py
+++ src/models/project.py
@@ -2,8 +2,8 @@ import enum
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import DateTime, String, Text, func
-from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy import ForeignKey, String, func
+from sqlalchemy.dialects.postgresql import ARRAY, ENUM
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database.base import Base
@@ -15,19 +15,19 @@ class Status(enum.Enum):
     inactive = "inactive"
 
 
-class Post(Base):
-    __tablename__ = "post"
+class Project(Base):
+    __tablename__ = "project"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    title: Mapped[str] = mapped_column(String(60))
-    author: Mapped[str]
-    slug: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
+    image: Mapped[str]
+    url: Mapped[str]
+    description: Mapped[str] = mapped_column(String(240))
+    tags: Mapped[Optional[list[str]]] = mapped_column(ARRAY(String))
     status: Mapped[Status] = mapped_column(ENUM(Status, name="status"))
-    description: Mapped[str]
-    content: Mapped[str] = mapped_column(Text())
     created_at: Mapped[Optional[datetime]] = mapped_column(insert_default=func.now())
     updated_at: Mapped[Optional[datetime]] = mapped_column(
         insert_default=func.now(), onupdate=func.now()
     )
-    published_at: Mapped[Optional[datetime]] = mapped_column(DateTime())
-    project: Mapped[Optional["Project"]] = relationship(back_populates="post")  # noqa
+    post_id: Mapped[Optional[int]] = mapped_column(ForeignKey("post.id"))
+    post: Mapped[Optional["Post"]] = relationship(back_populates="project")  # noqa
