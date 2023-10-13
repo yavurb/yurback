50ea9c1eedcc15d62c64c2243791612d041dcca3
Yurb <yavurb@gmail.com>
2023-10-13 10:48:15 -0400

    ✨ feat: add endpoint operation
    Include route for get-projects operation

--- src/api/endpoints/projects.py
+++ src/api/endpoints/projects.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from src.crud.project import project as crud
 from src.database.deps import get_db
+from src.schemas.generics import ResponseAsList
 from src.schemas.project import Project, ProjectCreate
 
 router = APIRouter()
@@ -16,3 +17,9 @@ def create_project(
 ) -> Project:
     created_project = crud.create(db, obj_in=project)
     return created_project
+
+
+@router.get("")
+def get_projects(db: Annotated[Session, Depends(get_db)]) -> ResponseAsList[Project]:
+    projects = crud.get_multi(db)
+    return {"data": projects}
