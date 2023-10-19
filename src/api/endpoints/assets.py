162fa0190e7f91d45a96b24f185853a5871b308e
Yurb <yavurb@gmail.com>
2023-10-19 10:46:49 -0400

    ♻️ refactor: add created assets to db

--- src/api/endpoints/assets.py
+++ src/api/endpoints/assets.py
@@ -1,14 +1,22 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
+from sqlalchemy.orm import Session
 
+from src.crud.asset import asset as crud
+from src.database.deps import get_db
 from src.lib.storage import Storage
+from src.schemas.asset import CreateAsset
 
 router = APIRouter()
 
 
 @router.post("")
-def upload_file(file: UploadFile, storage: Annotated[Storage, Depends(Storage)]):
+def upload_file(
+    file: UploadFile,
+    db: Annotated[Session, Depends(get_db)],
+    storage: Annotated[Storage, Depends(Storage)],
+):  # TODO: add return type
     is_uploaded, file_key = storage.upload(file.filename, file.file)
 
     if not is_uploaded:
@@ -16,4 +24,7 @@ def upload_file(file: UploadFile, storage: Annotated[Storage, Depends(Storage)])
             status.HTTP_400_BAD_REQUEST, detail={"message": "Could not upload file"}
         )
 
-    return {"filename": file_key}
+    asset = CreateAsset(key=file_key, mimetype=file.content_type)
+    create_asset = crud.create(db, obj_in=asset)
+
+    return {"id": create_asset.id, "filename": file_key}
